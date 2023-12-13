637b9e04891606b7635bcf44b816d104fa49f9b6
Ryan <ryan@christineandrick.com>
2023-12-13 18:03:16 -0500

    Fixed some TODOs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -14,13 +14,13 @@ package frc.robot;
  */
 public final class Constants {
   public static final class DriveConstants {
-    public static final int kLeftMotor1Port = 0;
-    public static final int kLeftMotor2Port = 1;
-    public static final int kRightMotor1Port = 2;
-    public static final int kRightMotor2Port = 3;
+    public static final int kLeftMotor1Port = 1;
+    public static final int kLeftMotor2Port = 2;
+    public static final int kRightMotor1Port = 3;
+    public static final int kRightMotor2Port = 4;
 
-    public static final int[] kLeftEncoderPorts = new int[] {0, 1};
-    public static final int[] kRightEncoderPorts = new int[] {2, 3};
+    public static final int[] kLeftEncoderPorts = new int[] {1, 2};
+    public static final int[] kRightEncoderPorts = new int[] {3, 4};
     public static final boolean kLeftEncoderReversed = false;
     public static final boolean kRightEncoderReversed = true;
 
@@ -36,7 +36,7 @@ public final class Constants {
     public static final double kStabilizationI = 0.5;
     public static final double kStabilizationD = 0;
 
-    public static final double kTurnP = 1;
+    public static final double kTurnP = 0.3;
     public static final double kTurnI = 0;
     public static final double kTurnD = 0;
 
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -4,31 +4,29 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.Constants.DriveConstants;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-//TODO: Change PWMSparkMax to CANSparkMax
-//TODO: Check CANid/port number of drive constants
-//TODO: Check encoder port numbers
 
 public class DriveSubsystem extends SubsystemBase {
   // The motors on the left side of the drive.
   private final MotorControllerGroup m_leftMotors =
       new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.kLeftMotor1Port),
-          new PWMSparkMax(DriveConstants.kLeftMotor2Port));
+          new CANSparkMax(DriveConstants.kLeftMotor1Port,MotorType.kBrushless),
+          new CANSparkMax(DriveConstants.kLeftMotor2Port, MotorType.kBrushless));
 
   // The motors on the right side of the drive.
   private final MotorControllerGroup m_rightMotors =
       new MotorControllerGroup(
-          new PWMSparkMax(DriveConstants.kRightMotor1Port),
-          new PWMSparkMax(DriveConstants.kRightMotor2Port));
+          new CANSparkMax(DriveConstants.kRightMotor1Port,MotorType.kBrushless),
+          new CANSparkMax(DriveConstants.kRightMotor2Port,MotorType.kBrushless));
 
   // The robot's drive
   private final DifferentialDrive m_drive = new DifferentialDrive(m_leftMotors, m_rightMotors);
